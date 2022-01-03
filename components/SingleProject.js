abc96abd82bb288c26428a901bdee31b835e3991
mosaab-rady <mosp.rady@gmail.com>
2022-01-03 18:35:34 +0200

    removed bugs

--- components/SingleProject.js
+++ components/SingleProject.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 export default function SingleProject({ project }) {
-  const [color, setColor] = useState(``);
+  const [color, setColor] = useState(`to-green-300`);
   useEffect(() => {
     setColor(`${project.color}`);
   }, [project]);
